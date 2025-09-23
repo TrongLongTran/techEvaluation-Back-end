c8b43b5ed0a34f8c5f7a72714bfab8d32f40f216
Long <ttran101@mylangara.ca>
2025-09-23 12:52:02 -0700

    Set up back end done

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require("express")
 const app = express();
+const mongoose = require("mongoose")
+const dotenv = require("dotenv")
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_KEY)
+.then(()=>{console.log("Connected to mongodb")})
+.catch((err)=>{console.log("Error, nuh uh, problem is: "), err})
+
+app.use(express.json())
 
 app.get("/api", (req, res)=>{
     res.json({fruits: ["new", "app"]})
